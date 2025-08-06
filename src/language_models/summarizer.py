ced29f174ff4b9e8ec1bc55e830b896b431bc0f8
Jai'Anni White <W214570534@student.hccs.edu>
2025-08-05 23:38:57 -0500

    Update summarizer.py

--- src/language_models/summarizer.py
+++ src/language_models/summarizer.py
@@ -1,1 +1,16 @@
+# src/language_models/summarizer.py
+
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+
+class ExtractiveSummarizer:
+    def __init__(self, num_sentences=3):
+        self.num_sentences = num_sentences
+
+    def summarize(self, text):
+        parser = PlaintextParser.from_string(text, Tokenizer("english"))
+        summarizer = LsaSummarizer()
+        summary = summarizer(parser.document, self.num_sentences)
+        return " ".join(str(sentence) for sentence in summary)
 
