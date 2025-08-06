a03314ae071156737e4ec79ef53d32ed1519588f
Jai'Anni White <W214570534@student.hccs.edu>
2025-08-05 23:41:30 -0500

    Update embeddings.py

--- src/language_models/embeddings.py
+++ src/language_models/embeddings.py
@@ -1,1 +1,14 @@
+# src/language_models/embeddings.py
+
+from sentence_transformers import SentenceTransformer
+
+class EmbeddingModel:
+    def __init__(self, model_name='all-MiniLM-L6-v2'):
+        self.model = SentenceTransformer(model_name)
+
+    def get_embedding(self, text):
+        return self.model.encode(text)
+
+    def get_batch_embeddings(self, texts):
+        return self.model.encode(texts, convert_to_tensor=True)
 
