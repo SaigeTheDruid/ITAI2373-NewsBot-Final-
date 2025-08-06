dbd3b4ab218c07c1300b86f0c0a86683a96c3d27
Jai'Anni White <W214570534@student.hccs.edu>
2025-08-06 16:12:12 -0500

    Update test_classification.py

--- tests/test_classification.py
+++ tests/test_classification.py
@@ -1,1 +1,15 @@
+import unittest
+from src.analysis.classifier import NewsClassifier
+
+class TestNewsClassifier(unittest.TestCase):
+    def test_train_predict(self):
+        X = ["AI is great", "Sports are fun", "Government news today"]
+        y = ["tech", "sports", "politics"]
+        model = NewsClassifier(model_type='svm')
+        model.train(X, y)
+        preds = model.predict(["AI breakthrough"])
+        self.assertEqual(len(preds), 1)
+
+if __name__ == '__main__':
+    unittest.main()
 
