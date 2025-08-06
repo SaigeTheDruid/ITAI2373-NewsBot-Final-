c14980a9fd4813e23cad56dbdad46088ba433504
Jai'Anni White <W214570534@student.hccs.edu>
2025-08-06 16:06:05 -0500

    Update data_validator.py

--- src/data_processing/data_validator.py
+++ src/data_processing/data_validator.py
@@ -1,1 +1,12 @@
+import pandas as pd
+
+class DataValidator:
+    def check_nulls(self, df):
+        return df.isnull().sum()
+
+    def check_column_types(self, df):
+        return df.dtypes
+
+    def check_unique_labels(self, df, label_col):
+        return df[label_col].nunique()
 
