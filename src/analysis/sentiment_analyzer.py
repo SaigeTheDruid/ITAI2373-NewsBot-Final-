489bc1d146f8979fd51c357a2d1105cb3e9afaec
Jai'Anni White <W214570534@student.hccs.edu>
2025-08-05 23:26:06 -0500

    Update sentiment_analyzer.py

--- src/analysis/sentiment_analyzer.py
+++ src/analysis/sentiment_analyzer.py
@@ -1,1 +1,14 @@
+from textblob import TextBlob
+
+class SentimentAnalyzer:
+    def analyze_sentiment(self, text):
+        blob = TextBlob(text)
+        polarity = blob.sentiment.polarity
+        subjectivity = blob.sentiment.subjectivity
+        sentiment = 'positive' if polarity > 0 else 'negative' if polarity < 0 else 'neutral'
+        return {
+            'polarity': polarity,
+            'subjectivity': subjectivity,
+            'sentiment': sentiment
+        }
 
