4acfa041655bbdbd72a610be8fd17a3054bc926f
Jai'Anni White <W214570534@student.hccs.edu>
2025-08-05 22:42:22 -0500

    Update text_preprocessor.py

--- src/data_processing/text_preprocessor.py
+++ src/data_processing/text_preprocessor.py
@@ -1,1 +1,51 @@
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+# Download NLTK resources
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+
+class TextPreprocessor:
+    def __init__(self):
+        self.stop_words = set(stopwords.words('english'))
+        self.lemmatizer = WordNetLemmatizer()
+
+    def clean_text(self, text):
+        text = text.lower()
+        text = re.sub(r"http\\S+|www\\S+", "", text)
+        text = text.translate(str.maketrans('', '', string.punctuation))
+        tokens = nltk.word_tokenize(text)
+        tokens = [t for t in tokens if t.isalpha()]
+        tokens = [t for t in tokens if t not in self.stop_words]
+        tokens = [self.lemmatizer.lemmatize(t) for t in tokens]
+        return " ".join(tokens)
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+# Download NLTK resources
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+
+class TextPreprocessor:
+    def __init__(self):
+        self.stop_words = set(stopwords.words('english'))
+        self.lemmatizer = WordNetLemmatizer()
+
+    def clean_text(self, text):
+        text = text.lower()
+        text = re.sub(r"http\\S+|www\\S+", "", text)
+        text = text.translate(str.maketrans('', '', string.punctuation))
+        tokens = nltk.word_tokenize(text)
+        tokens = [t for t in tokens if t.isalpha()]
+        tokens = [t for t in tokens if t not in self.stop_words]
+        tokens = [self.lemmatizer.lemmatize(t) for t in tokens]
+        return " ".join(tokens)
 
