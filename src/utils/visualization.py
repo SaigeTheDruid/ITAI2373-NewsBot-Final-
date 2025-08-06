ec1cf353c1a8d4d0db2d0358391d4bfb6dfde5c2
Jai'Anni White <W214570534@student.hccs.edu>
2025-08-06 17:23:52 -0500

    Update visualization.py

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -1,1 +1,18 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud
+from sklearn.metrics import ConfusionMatrixDisplay
+
+def plot_wordcloud(text):
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+    plt.figure(figsize=(15, 7))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.title("Word Cloud")
+    plt.show()
+
+def plot_confusion_matrix(conf_matrix, labels):
+    disp = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=labels)
+    disp.plot(cmap='Blues')
+    plt.show()
 
