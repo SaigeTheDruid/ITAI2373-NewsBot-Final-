32fac3cc584ded952f3bd71cb7f2f735d3f322a7
Jai'Anni White <W214570534@student.hccs.edu>
2025-08-06 16:11:57 -0500

    Update test_preprocessing.py

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,1 +1,15 @@
+import unittest
+from src.data_processing.text_preprocessor import TextPreprocessor
+
+class TestTextPreprocessor(unittest.TestCase):
+    def test_clean_text(self):
+        cleaner = TextPreprocessor()
+        text = "Hello!!! This is a TEST -- of www.google.com, with URLs and punctuation."
+        cleaned = cleaner.clean_text(text)
+        self.assertIsInstance(cleaned, str)
+        self.assertNotIn("http", cleaned)
+        self.assertTrue("test" in cleaned.lower())
+
+if __name__ == '__main__':
+    unittest.main()
 
