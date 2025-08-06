188a18a28b296420e4a115b8aa9dbcdc57db1022
Jai'Anni White <W214570534@student.hccs.edu>
2025-08-06 17:24:55 -0500

    Update export.py

--- src/utils/export.py
+++ src/utils/export.py
@@ -1,1 +1,10 @@
+import json
+import pandas as pd
+
+def export_classification_report(report, filepath):
+    with open(filepath, 'w') as f:
+        json.dump(report, f, indent=4)
+
+def export_df_to_csv(df, filepath):
+    df.to_csv(filepath, index=False)
 
